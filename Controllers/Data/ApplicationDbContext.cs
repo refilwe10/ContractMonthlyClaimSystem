9d8b1747f0011ed955925df5bc0593bad1b020d2
Refilwe Phore <refilwephore10@gmail.com>
2025-11-18 08:20:21 +0200

    added coordinatorController

--- Controllers/Data/ApplicationDbContext.cs
+++ Controllers/Data/ApplicationDbContext.cs
@@ -9,7 +9,7 @@ namespace ContractMonthlyClaimSystem.Controllers.Data
         public DbSet<Lecturer> Lecturers { get; set; }
         public DbSet<Claim> Claims { get; set; }
         public DbSet<ProgrammeCoordinator> ProgrammeCoordinators { get; set; } 
-
+         
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { } 
     }
 }
